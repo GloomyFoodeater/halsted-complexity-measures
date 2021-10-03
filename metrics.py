db353f703f46c37a8f31669dfb2f90800818c85f
GloomyFoodeater <s.paulavets@gmail.com>
2021-10-03 16:29:02 +0300

    Add metrics class interface

---
+++ metrics.py
@@ -0,0 +1,15 @@
+class HolstedMeasures:
+    def __init__(self, src):
+        pass
+ 
+    def _get_operands(self):
+        pass
+    def _get_operators(self):
+        pass
+    
+    @property
+    def operand_vocabulary(self):
+        pass
+    @property
+    def operator_vocabulary(self):
+        pass
